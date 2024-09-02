05c962c281b1f3f26f6474bada686261c17d3c8f
sddpltwanqiu <sddpltwanqiu@163.com>
2024-09-02 11:28:05 +0800

    Update TGS_dataprepare.py

--- TGS_score_model/TGS_dataprepare.py
+++ TGS_score_model/TGS_dataprepare.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     dp = DataPreparation(
         label_path='../dataset/forest_label/Hansen_1km_th30.tif',
         input_parameter_dir='../dataset/environmental',
-        output_dir= '../dataset_usefortrain/'
+        output_dir= '../dataset/UseForTraning/'
     )
     data = dp.preprocess_data()
     dp.save_splits(data)
